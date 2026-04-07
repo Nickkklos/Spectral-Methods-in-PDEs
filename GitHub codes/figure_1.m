0ce503acf3c1d25e7573494342512c414e7aef4f
胡屹洋 <hyiyang882@gmail.com>
2026-04-07 01:35:49 +0100

    Adjust N values for erfc calculations in figure_1.m

--- GitHub codes/figure_1.m
+++ GitHub codes/figure_1.m
@@ -1,14 +1,14 @@
 % erfc
-N  = (1:8).';
+N  = (4:2:14).';
 t1 = 0.1;
 t2 = 0.5;
 t3 = 1.0;
 
-J01 = (pi/(4*t1))^0.5*erfc((N)*sqrt(t1));
-J05 = (pi/(4*t2))^0.5*erfc((N)*sqrt(t2));
-J1  = (pi/(4*t3))^0.5*erfc((N)*sqrt(t3));
+J01 = (pi/(4*t1))^0.5*erfc((N/2)*sqrt(t1));
+J05 = (pi/(4*t2))^0.5*erfc((N/2)*sqrt(t2));
+J1  = (pi/(4*t3))^0.5*erfc((N/2)*sqrt(t3));
 
 T = table(N, J01, J05, J1, ...
-    'VariableNames', {'N','J(0.1,N)','J(0.5,N)','J(1,N)'});
+    'VariableNames', {'N','J(0.1,N/2)','J(0.5,N/2)','J(1,N/2)'});
 
 disp(T)
